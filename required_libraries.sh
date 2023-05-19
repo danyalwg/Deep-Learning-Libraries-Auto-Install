cd5902d33a5ab2f3becccc7e291d7da964d8cb69
danyalwg <68191777+danyalwg@users.noreply.github.com>
2023-05-19 10:25:45 +0500

    Add files via upload
    ------------------------------------------------------------------
    Welcome to the Guide for Software Installation for Computer Vision
    ------------------------------------------------------------------
    
    This guide will help you install a list of required software and libraries for Computer Vision and Deep Learning development on Ubuntu.
    
    The name of the script is 'required_libraries.sh'.
    
    Steps to Run the Script
    -----------------------
    
    1. Opening the Terminal:
    
       First, you will need to open the terminal. You can open the terminal by searching for 'terminal' in the system dash or by using the keyboard shortcut 'Ctrl+Alt+T'.
    
    2. Navigating to the script:
    
       Once the terminal is open, you will need to navigate to the directory (folder) where the script 'required_libraries.sh' is saved.
    
       You can navigate in terminal using the 'cd' command which stands for 'change directory'. For example, if your script is in a folder on the Desktop called 'scripts', you would type:
    
       cd Desktop/scripts
    
       and press Enter. You'll now be in the scripts folder.
    
    3. Running the Script:
    
       Before running the script, you need to make sure that it has the necessary permissions. In the terminal, type the following command:
    
       chmod +x required_libraries.sh
    
       and press Enter. This command changes the script to be executable, which means you can now run it.
    
       Now, to run the script, type:
    
       ./required_libraries.sh
    
       and press Enter. 
    
       The script will now begin running. Depending on your system's speed and the current state of installed software, it may take some time to complete. 
    
    Please note that this script requires administrator (sudo) access, and will run commands as such. It is important to only run scripts as administrator if they are from a trusted source, as they have high-level access to your system.
    
    Remember: You can stop the script at any time by pressing 'Ctrl+C' in the terminal.
    
    In case of any error messages or if the script fails to run, make sure the command was typed correctly, and that the 'required_libraries.sh' script is indeed in the directory you navigated to.
    
    If you are still facing issues, you may need to consult further documentation or support for your specific system configuration.

---
+++ required_libraries.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Update and upgrade system
+sudo apt update -y
+sudo apt upgrade -y
+
+# Install cmake
+sudo apt install -y cmake
+
+# Install python3 and pip
+sudo apt install -y python3 python3-pip
+
+# Install Python libraries
+pip3 install --upgrade numpy scipy pandas matplotlib seaborn opencv-python-headless scikit-image pillow
+
+# Install deep learning libraries
+pip3 install --upgrade tensorflow keras torch torchvision theano
+
+# Install IDEs and code editors
+sudo snap install --classic pycharm-community
+pip3 install --upgrade jupyter
+sudo snap install --classic code
+
+# Sublime Text 3 install with apt-get
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+# Install Git
+sudo apt install -y git
+
+# Install Docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+echo "Installation complete!"
